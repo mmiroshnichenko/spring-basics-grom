d6eb248c19bec5432bcb29e46f65807b532909b3
Mykhaylo Myroshnychenko <michael.miroshnichenko@gmail.com>
2019-07-02 21:08:37 +0300

    lesson7. hw

--- src/main/java/com/lesson7_1/controller/AdvertisementController.java
+++ src/main/java/com/lesson7_1/controller/AdvertisementController.java
@@ -1,8 +1,12 @@
 package com.lesson7_1.controller;
 
+import com.lesson7_1.entity.Advertisement;
 import com.lesson7_1.service.AdvertisementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class AdvertisementController extends BaseController {
@@ -13,5 +17,52 @@ public class AdvertisementController extends BaseController {
         this.advertisementService = advertisementService;
     }
 
+    @RequestMapping(method = RequestMethod.POST, value = "/advertisement/save", produces = "text/plain")
+    public @ResponseBody
+    String save(HttpSession session, @RequestBody Advertisement advertisement) {
+        try {
+            checkAuthentication(session);
 
+            return advertisementService.save(advertisement).toString();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/advertisement/update", produces = "text/plain")
+    public @ResponseBody
+    String update(HttpSession session, @RequestBody Advertisement advertisement) {
+        try{
+            checkAuthentication(session);
+
+            return advertisementService.update(advertisement).toString();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/advertisement/delete", params = {"id"}, produces = "text/plain")
+    public @ResponseBody
+    String delete(HttpSession session, @RequestParam(value = "id") String id) {
+        try {
+            checkAuthentication(session);
+
+            advertisementService.delete(Long.parseLong(id));
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+        return "deleting ok";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/user/get", params = {"id"}, produces = "text/plain")
+    public @ResponseBody
+    String get(HttpSession session, @RequestParam(value = "id") String id) {
+        try {
+            checkAuthentication(session);
+
+            return advertisementService.findById(Long.parseLong(id)).toString();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
 }
--- src/main/java/com/lesson7_1/service/AdvertisementService.java
+++ src/main/java/com/lesson7_1/service/AdvertisementService.java
@@ -3,10 +3,15 @@ package com.lesson7_1.service;
 import com.lesson7_1.DAO.AdvertisementDAO;
 import com.lesson7_1.DAO.UserDAO;
 import com.lesson7_1.entity.Advertisement;
+import com.lesson7_1.exception.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 @Transactional
@@ -22,6 +27,11 @@ public class AdvertisementService {
 
     public Advertisement save(Advertisement advertisement) throws Exception {
         validate(advertisement);
+        Calendar cal = Calendar.getInstance();
+        advertisement.setDateCreated(cal.getTime());
+        cal.add(Calendar.DATE, 30);
+        Date expiredDate = cal.getTime();
+        advertisement.setDateExpired(expiredDate);
 
         return advertisementDAO.save(advertisement);
     }
@@ -53,7 +63,45 @@ public class AdvertisementService {
         return advertisementDAO.findById(id);
     }
 
-    private void validate(Advertisement advertisement) {
+    private void validate(Advertisement advertisement) throws BadRequestException{
+        if (advertisement.getTitle() == null || advertisement.getTitle().isEmpty()) {
+            throw new BadRequestException("Error: title is required");
+        }
 
+        if (advertisement.getDescription().length() > 200) {
+            throw new BadRequestException("Error: description must be less that 200");
+        }
+
+        if (advertisement.getPrice() < 0) {
+            throw new BadRequestException("Error: incorrect price");
+        }
+
+        if (advertisement.getCurrencyEnum() == null) {
+            throw new BadRequestException("Error: incorrect currency");
+        }
+
+        if (advertisement.getCategoryEnum() == null) {
+            throw new BadRequestException("Error: incorrect category");
+        }
+
+        if (advertisement.getSubcategoryEnum() == null) {
+            throw new BadRequestException("Error: incorrect subcategory");
+        }
+
+        if (advertisement.getCity() == null) {
+            throw new BadRequestException("Error: city is required");
+        }
+
+        if (advertisement.getPhone() == null) {
+            throw new BadRequestException("Error: phone is required");
+        }
+
+        String regex = "[0-9*#+() -]*";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(advertisement.getPhone());
+
+        if(!matcher.matches()) {
+            throw new BadRequestException("Error: incorrect phone format");
+        }
     }
 }
